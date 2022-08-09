3f714e5e369f249cdeeaf517ac7b42397ac1a79b
miyakiyu <sherry910509@gmail.com>
2022-08-09 19:25:47 +0800

    Add something to let the game stop
    Add if the map full the game will over and we finally can stop game
    now thanks to the Nekokingu

--- ooxx.kt
+++ ooxx.kt
@@ -36,8 +36,18 @@ fun main()
             {
                 println(map[i].toString())
             }
-            chooser+=1
-            println(check_if_win(map))
+
+            //check win 
+            if(check_if_win(map)==true)
+            {
+                game_end_check =false
+                println("O WIN!!! (b･O ･)b")
+            }
+            else if(check_draw(map)==true)
+            {
+                game_end_check = false  
+            }
+            else chooser+=1
         }
 
         //######################## I'M SPLIT LINE ########################//
@@ -57,39 +67,64 @@ fun main()
             {
                 println(map[i].toString())
             }
-            chooser+=1
 
-            //let the game end
-            if(chooser == 8)
+            //check win 
+            if(check_if_win(map)==true)
             {
-                game_end_check = false
-            }   
-            println(check_if_win(map))
+                println("X WIN!!! (b･X ･)b")
+            }
+            else if(check_draw(map)==true)
+            {
+                game_end_check = false  
+            }
+            else chooser+=1
         }
-
-        
     }  
 }
 
-fun check_if_win(map:ArrayList<ArrayList<String>>):String
+fun check_if_win(map:ArrayList<ArrayList<String>>):Boolean
 {   
-    //check row and column
+    //check row 
     for(i in 0..2)
     {
         if(map[i][0] == map[i][1] && map[i][1] == map[i][2] && map[i][0] != "-") 
         {
-            return "YOU WIN!"
+            return true
         }
         else if(map[0][i] == map[1][i] && map[1][i] == map[2][i] && map[0][i] != "-")
         {
-            return "YOU WIN!"
+            return true
         }
         else break
     }
 
-    
-    //check 
-
-    return " "
+    //check column
+    for(i in 0..2)
+    {
+        if(map[0][0] == map[1][1] && map[1][1] == map[2][2] && map[0][0] != "-")
+        {
+            return true
+        }
+        else if(map[2][0] == map[1][1] && map[1][1] == map[0][2] && map[2][0] != "-")
+        {
+            return true
+        }
+        else break
+    }
+    return false
 }
 
+fun check_draw(map:ArrayList<ArrayList<String>>):Boolean
+{
+    for(i in 0..2)
+    {
+        for(j in 0..2)
+        {
+            if(map[i][j] == "-")
+            {
+                return false
+            }
+        }
+    }
+    return true
+}
